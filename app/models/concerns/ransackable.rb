df79b3c736c0993249bf258762f77e445449ace7
Dmitriy Paramonov <divniy@gmail.com>
2025-07-08 14:47:14 +0000

    add posts AA resource

---
+++ app/models/concerns/ransackable.rb
@@ -0,0 +1,34 @@
+module Ransackable
+  extend ActiveSupport::Concern
+
+  class_methods do
+    # `ransackable_attributes` returns searchable column names
+    # and any defined ransackers as an array of strings.
+    #
+    def ransackable_attributes(auth_object = nil)
+      column_names + _ransackers.keys
+    end
+
+    # `ransackable_associations` returns the names
+    # of searchable associations as an array of strings.
+    #
+    def ransackable_associations(auth_object = nil)
+      [] #%w[author]
+    end
+
+    # `ransortable_attributes` by default returns the names
+    # of all attributes available for sorting as an array of strings.
+    #
+    def ransortable_attributes(auth_object = nil)
+      ransackable_attributes(auth_object)
+    end
+
+    # `ransackable_scopes` by default returns an empty array
+    # i.e. no class methods/scopes are authorized.
+    # For overriding with an allowlist, return an array of *symbols*.
+    #
+    def ransackable_scopes(auth_object = nil)
+      []
+    end
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  include Ransackable
   belongs_to :author, class_name: "User"
   has_rich_text :body
 end
