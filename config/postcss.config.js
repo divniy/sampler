796b437be7be7de906ad55723b0ebec8ee55f79f
Dmitriy Paramonov <divniy@gmail.com>
2025-07-09 15:29:35 +0000

    move config away from root

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss/nesting'),
-    require('tailwindcss'),
+    require('tailwindcss')({ config: './config/tailwind-active_admin.config.js' }),
     require('autoprefixer'),
   ],
 }
